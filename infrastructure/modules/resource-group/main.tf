5ec0b588903bdc6d375137f37da3509c36129b5c
ShanthiTV <113419029+ShanthiTV@users.noreply.github.com>
2023-06-05 22:58:05 +0530

    Update main.tf

--- infrastructure/modules/resource-group/main.tf
+++ infrastructure/modules/resource-group/main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "adl_rg1" {
+resource "azurerm_resource_group" "adl_rg" {
   name     = "rg-${var.prefix}-${var.postfix}${var.env}"
   location = var.location
   tags     = var.tags
